a08a0b0bd8952a26dc32be345381d086a98b4550
Vitalii Pikozh <wisart@gmail.com>
2025-11-30 11:54:46 +0200

    feat(parser): add Accept-Language header for Ukrainian localization

--- utils/parseEvent.js
+++ utils/parseEvent.js
@@ -31,6 +31,7 @@ export async function parseEvent(eventId) {
       headers: {
         "User-Agent":
           "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 Chrome/120",
+        "Accept-Language": "uk-UA,uk;q=0.9",
       },
     });
 
